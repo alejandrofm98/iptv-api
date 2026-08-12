6312e4959e3352a5036d9f3bb369b63d43e6bc2d
alejandrofm98 <alejandrofmayor98@gmail.com>
2026-08-12 22:02:57 +0200

    chore: parameterize dev deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 # Aplicar migraciones pendientes de iptv-db
 cd /opt/iptv-db
-git pull origin main 2>/dev/null || echo "iptv-db pull skipped, using built-in version"
 
 python3 -m alembic upgrade head
 
